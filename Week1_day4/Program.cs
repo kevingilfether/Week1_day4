88a5eddd524c5a93689b5cc8483749577dcd9d90
kevingilfether <kevingilfether@gmail.com>
2017-09-28 13:45:06 -0400

    update program with some concats

--- Week1_day4/Program.cs
+++ Week1_day4/Program.cs
@@ -10,6 +10,32 @@ namespace Week1_day4
     {
         static void Main(string[] args)
         {
+            // A string is a sequence of characters inside 
+            // inside "double quotes"
+
+            //Console.WriteLine("This is a tab \t.");
+            //Console.WriteLine("This is a line \"quote.\"");
+            //Console.WriteLine("This is a backslash \\");
+            //Console.WriteLine("This is a \n new line");
+            //Console.WriteLine("And this is a bell \a");
+
+            //Man, that bell is annoying.
+
+
+            // Cool way to concatenate using indexes. {0} = zero index
+            string firstName = "Kevin";
+            string catName = "Nugget";
+
+            //Console.WriteLine("My first name is {0} and my cat is {1}.", firstName, catName);
+
+            // Cool way to concatenate using variables
+            //string bestFriends = firstName + " " + catName;
+            //Console.WriteLine(bestFriends);
+
+            //Obscure rare way to concatenate
+
+           //Console.WriteLine(string.Concat(firstName, catName));
+            
         }
     }
 }
